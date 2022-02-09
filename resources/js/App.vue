fd675ec7932540e96514d798e910f9624ce86e6c
Mdigennaro <michele.digennaro719@gmail.com>
2022-02-09 16:42:16 +0100

    creata struttura dell'header e del footer con vue

---
+++ resources/js/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Header  />
+    <main>Home Vue</main>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './components/partials/Header.vue';
+import Footer from './components/partials/Footer.vue';
+export default {
+  name: "App",
+  
+  components:{
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+
+</style>
